4590a400d2c4173960a32eaa92444aa4cad4b9da
TelesNascimento25 <Darlyssonteles98@outlook.com>
2023-06-07 08:49:19 -0300

    produzindo o site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './componente/menu-bar/menu-bar.component';
+import { MenuTitleComponent } from './componente/menu-title/menu-title.component';
+import { BigCardComponent } from './componente/big-card/big-card.component';
+import { SmallCardComponent } from './componente/small-card/small-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    MenuTitleComponent,
+    BigCardComponent,
+    SmallCardComponent
   ],
   imports: [
     BrowserModule,
